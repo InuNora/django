1992879b88bb2cf117f4bbdd0d6590e37c19bdb8
Julia Pikareva <sharp@ngs.ru>
2016-05-12 23:30:36 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 import socket
 import asyncore
 
+def  server(conn, addr):
+    while True:
+        data = conn.recv(1024)
+        #data = myreceive(s, 1024)
+        if not data: break
+        if data == 'close': break
+        conn.send(data)
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
 while True:
     conn, addr = s.accept()
-    while True:
-        data = conn.recv(1024)
-        if not data: break
-        if data == 'close': break
-        conn.send(data)
+    server(conn, addr)
     conn.close()
