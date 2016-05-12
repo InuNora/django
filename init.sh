7825d78780f6d7ef8301640cc30502eb8e0c8783
Julia Pikareva <sharp@ngs.ru>
2016-05-12 23:53:56 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+
 mkdir -p /home/box/web/public
 mkdir -p /home/box/web/public/img
 mkdir -p /home/box/web/public/css
