7f183a8b0f02c7056c2efc5993825ab74388da24
Julia Pikareva <sharp@ngs.ru>
2016-05-12 23:46:33 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+mkdir -p /home/box/web/public
+mkdir -p /home/box/web/public/img
+mkdir -p /home/box/web/public/css
+mkdir -p /home/box/web/public/js
+mkdir -p /home/box/web/uploads
+mkdir -p /home/box/web/etc
