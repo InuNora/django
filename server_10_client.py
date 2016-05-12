3ec629e76bf160080053bd0a963b74765c7b481a
Julia Pikareva <sharp@ngs.ru>
2016-05-12 23:02:04 +0300

    Create server_10_client.py

---
+++ server_10_client.py
@@ -0,0 +1,24 @@
+import socket
+import asyncore
+
+def myreceive(sock, msglen):
+    msg = ''
+    while len(msg) < msglen:
+        chunk = sock.recv(msglen-len(msg))
+        if chunk == '':
+            raise RuntimeError("broken")
+        msg = msg + chunk
+    return msg
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(10)
+while True:
+    conn, addr = s.accept()
+    while True:
+        #data = conn.recv(1024)
+        data = myreceive(s, 1024)
+        if not data: break
+        conn.send(data)
+    conn.close()
+    
