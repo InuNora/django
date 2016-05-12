c744d27bff5dbf769d1366a189aa8f20749b6961
Julia Pikareva <sharp@ngs.ru>
2016-05-12 23:55:56 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-
 mkdir -p /home/box/web/public
 mkdir -p /home/box/web/public/img
 mkdir -p /home/box/web/public/css
 mkdir -p /home/box/web/public/js
 mkdir -p /home/box/web/uploads
 mkdir -p /home/box/web/etc
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
