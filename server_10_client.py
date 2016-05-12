76ceb3f4f6cdab54c4c00b73c86a36779145e5e9
Julia Pikareva <sharp@ngs.ru>
2016-05-12 23:15:48 +0300

    Update server_10_client.py

--- server_10_client.py
+++ server_10_client.py
@@ -1,5 +1,6 @@
 import socket
 import asyncore
+import threading
 
 def myreceive(sock, msglen):
     msg = ''
@@ -9,16 +10,22 @@ def myreceive(sock, msglen):
             raise RuntimeError("broken")
         msg = msg + chunk
     return msg
+    
+def  server(conn, addr):
+    while True:
+        #data = conn.recv(1024)
+        data = myreceive(s, 1024)
+        if not data: break
+        conn.send(data)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
 while True:
     conn, addr = s.accept()
-    while True:
-        #data = conn.recv(1024)
-        data = myreceive(s, 1024)
-        if not data: break
-        conn.send(data)
+    print("Connection", addr)
+    t = threading.Thread(target=server, args=(conn, addr))
+    t.start()
+    
     conn.close()
     
