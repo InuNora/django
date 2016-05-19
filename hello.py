4720e4333b601597caf92f4370c795e9017b390d
Julia Pikareva <sharp@ngs.ru>
2016-05-19 19:15:34 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,13 @@ pythonpath = "/home/box/web"
 errorlog = "/home/box/web/etc/error.log"
 loglevel = "debug"
 def wsgi_application(environ, start_response):
-    #
+    #QUERY_STRING
+    #input: 
+    #/?a=1&a=2&b=3
+    #Output:
+    #a=1
+    #a=2
+    #b=3
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
