62fbd899065c2550cc2870e54bb33a8e83e90ba5
Julia Pikareva <sharp@ngs.ru>
2016-05-19 21:17:54 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,7 @@
-bind = "0.0.0.0:8080"
-workers = 1
-pythonpath = "/home/box/web"
-errorlog = "/home/box/web/etc/error.log"
-loglevel = "debug"
+
 def wsgi_application(environ, start_response):
-    #QUERY_STRING
+    mas = environ['QUERY_STRING'].split("&")
+    body = mas.join("\n")
     #input: 
     #/?a=1&a=2&b=3
     #Output:
@@ -15,6 +12,6 @@ def wsgi_application(environ, start_response):
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    body = 'Hello, world!'
+    #body = 'Hello, world!'
     start_response(status, headers )
     return [ body ]
