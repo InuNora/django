a82873a108c1c0cb2adb01cc1c1b71ca64fcfa60
Julia Pikareva <sharp@ngs.ru>
2016-05-19 21:20:36 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,17 +1,10 @@
 
 def wsgi_application(environ, start_response):
     mas = environ['QUERY_STRING'].split("&")
-    body = mas.join("\n")
-    #input: 
-    #/?a=1&a=2&b=3
-    #Output:
-    #a=1
-    #a=2
-    #b=3
+    body = "\n".join(mas)
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    #body = 'Hello, world!'
     start_response(status, headers )
     return [ body ]
