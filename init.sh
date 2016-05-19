e1f1c2a1cdd7137bef48fff64202e0673ce6a9bd
Julia Pikareva <sharp@ngs.ru>
2016-05-19 21:28:39 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@
 mkdir -p /home/box/web/{etc,uploads,public}/
 mkdir -p /home/box/web/public/{img,js,css}/
 
-cp /home/box/web/stepic/nginx.conf /home/box/web/etc/nginx.conf
-cp /home/box/web/stepic/hello.py /home/box/web/hello.py
-cp /home/box/web/stepic/etc/hello.py /home/box/web/etc/hello.py
+cp /home/box/stepic/nginx.conf /home/box/web/etc/nginx.conf
+cp /home/box/stepic/hello.py /home/box/web/hello.py
+cp /home/box/stepic/etc/hello.py /home/box/web/etc/hello.py
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
