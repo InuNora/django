8bd0fab0871c00b8fa9d2c9a5a955ee99af64c5f
Julia Pikareva <sharp@ngs.ru>
2016-05-19 19:11:57 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,12 @@ workers = 1
 pythonpath = "/home/box/web"
 errorlog = "/home/box/web/etc/error.log"
 loglevel = "debug"
+def wsgi_application(environ, start_response):
+    #
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    body = 'Hello, world!'
+    start_response(status, headers )
+    return [ body ]
