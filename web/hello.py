1ffbac5d53c630f4451f6ca94f35ca6280620911
InuNora <sharp@ngs.ru>
2016-05-20 20:42:27 +0000

    Add all

---
+++ web/hello.py
@@ -0,0 +1,10 @@
+
+def application(environ, start_response):
+    mas = environ['QUERY_STRING'].split("&")
+    body = "\n".join(mas)
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    start_response(status, headers )
+    return [ body ]
