f66f13880acc17e9e399657611164d121f500793
Julia Pikareva <sharp@ngs.ru>
2016-05-20 22:46:11 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,5 +10,5 @@ cp /home/box/stepic/etc/hello.py /home/box/web/etc/hello.py
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-#sudo gunicorn - b 0.0.0.0:8080 hello:app
+gunicorn - b 0.0.0.0:8080 hello:application
 sudo /etc/init.d/gunicorn restart
