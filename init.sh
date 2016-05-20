b7ff428672f232722d66ed7eb9f3924dd3a93d54
Julia Pikareva <sharp@ngs.ru>
2016-05-20 23:08:56 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,6 +13,7 @@ cp /home/box/stepic/etc/hello.py /home/box/web/etc/hello.py
 # django-admin startapp qa
 
 cp /home/box/stepic/views.py /home/box/web/ask/qa/views.py
+cp /home/box/stepic/urls.py /home/box/web/ask/qa/urls.py
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
