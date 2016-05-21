770171516c587d3d6a22491cb39a55b3dc8f0c05
Julia Pikareva <sharp@ngs.ru>
2016-05-21 14:23:17 +0300

    Update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-gunicorn -c /home/box/web/etc/hello.py -b 0.0.0.0:8080 hello:application &
+# gunicorn -c /home/box/web/etc/hello.py -b 0.0.0.0:8080 hello:application &
 # gunicorn - b 0.0.0.0:8080 hello:application &
 
-cd /home/box/web/ask/ask
-gunicorn -c /home/box/web/etc/hello.py -b 0.0.0.0:8000 wsgi:application &
+cd /home/box/web/ask
+gunicorn -c gunicorn.ask.conf -b 0.0.0.0:8000 ask.wsgi &
+# gunicorn -c /home/box/web/etc/hello.py -b 0.0.0.0:8000 wsgi:application &
 # gunicorn - b 0.0.0.0:8000 wsgi:application &
