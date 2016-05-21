d5755c9a06744b708182a2b13d30a9421b7e22e6
Julia Pikareva <sharp@ngs.ru>
2016-05-21 14:12:59 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-cp /home/box/stepic/gunicorn.ask /home/box/web/etc/gunicorn.ask
-sudo ln -sf /home/box/web/etc/gunicorn.ask /etc/gunicorn.d/test/gunicorn.conf
+# cp /home/box/stepic/gunicorn.ask /home/box/web/etc/gunicorn.ask
+# sudo ln -sf /home/box/web/etc/gunicorn.ask /etc/gunicorn.d/test/gunicorn.conf
+sudo ln   -sf   /home/box/web/etc/gunicorn.ask.conf      /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
