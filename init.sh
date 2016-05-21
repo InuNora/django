4cbc7496297e14ae87f1d187ea56a68cbcabb888
Julia Pikareva <sharp@ngs.ru>
2016-05-21 13:36:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 
-mkdir -p /home/box/web/{etc,uploads,public}/
-mkdir -p /home/box/web/public/{img,js,css}/
-
-cp /home/box/stepic/nginx.conf.new /home/box/web/etc/nginx.conf
-cp /home/box/stepic/hello.py /home/box/web/hello.py
-cp /home/box/stepic/etc/hello.py /home/box/web/etc/hello.py
-
-cd /home/box/web/
-django-admin startproject ask
-cd /home/box/web/ask/
-django-admin startapp qa
-
-cp /home/box/stepic/views.py /home/box/web/ask/qa/views.py
-cp /home/box/stepic/urls.py /home/box/web/ask/qa/urls.py
-
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-# gunicorn -c /home/box/web/etc/hello.py -b 0.0.0.0:8080 hello:hello_app &
-# gunicorn - b 0.0.0.0:8080 hello:application &
-
-# cd /home/box/web/ask/ask
-# gunicorn -c /home/box/web/etc/hello.py -b 0.0.0.0:8000 wsgi:application &
-# gunicorn - b 0.0.0.0:8000 wsgi:application &
 cp /home/box/stepic/gunicorn.ask /home/box/web/etc/gunicorn.ask
 sudo ln -sf /home/box/web/etc/gunicorn.ask /etc/gunicorn.d/test/gunicorn.conf
 sudo /etc/init.d/gunicorn restart
