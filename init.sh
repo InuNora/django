a7354b9acbc98e140e4d88c2f3b9af4fb07abcb3
Julia Pikareva <sharp@ngs.ru>
2016-05-23 14:07:30 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-gunicorn -c /home/box/web/etc/hello.py hello:applicatoin --daemon
+gunicorn -c /home/box/web/etc/hello.py hello:application --daemon
 gunicorn -c /home/box/web/etc/django.py wsgi --daemon
