6b9731fed1a13c01b860e35e0cdc185ccd7585ce
Julia Pikareva <sharp@ngs.ru>
2016-05-23 21:56:36 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa',
+    'ask',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +59,11 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ask',
+        'USER': 'ask',
+        'PASSWORD': 'ask',
+        'HOST': 'localhost'
     }
 }
 
