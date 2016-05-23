208228622655cff48972f41a62478b784ef74bef
Julia Pikareva <sharp@ngs.ru>
2016-05-23 22:09:45 +0300

    Update vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -6,9 +6,13 @@ apt-get install -y Nginx
 apt-get install -y gunicorn
 apt-get install -y python-pip
 pip install Django==1.6.1
+apt-get install mc
+apt-get install mysql-server
 
 ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
 
 gunicorn -c /home/box/web/etc/hello.py hello:application --daemon
 gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+
+/etc/init.d/mysql start
