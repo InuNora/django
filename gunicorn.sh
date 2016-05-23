2a35e461f49c9abdbc4202b13ab93ad75c2a7195
Julia Pikareva <sharp@ngs.ru>
2016-05-23 13:21:21 +0300

    Delete gunicorn.sh

--- gunicorn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# gunicorn -c /home/box/web/etc/hello.py -b 0.0.0.0:8080 hello:application &
-# gunicorn - b 0.0.0.0:8080 hello:application &
-
-cd /home/box/web/ask
-gunicorn -c gunicorn.ask.conf -b 0.0.0.0:8000 ask.wsgi &
-# gunicorn -c /home/box/web/etc/hello.py -b 0.0.0.0:8000 wsgi:application &
-# gunicorn - b 0.0.0.0:8000 wsgi:application &
