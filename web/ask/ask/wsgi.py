79f7cf38457d6611988062127254a967d7b5278a
Julia Pikareva <sharp@ngs.ru>
2016-05-23 13:10:13 +0300

    Update wsgi.py

--- web/ask/ask/wsgi.py
+++ web/ask/ask/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
 from django.core.wsgi import get_wsgi_application
