5969b0349dcad12a8ebb84c2d8ebbaee6466e727
Julea Pikareva <sharp@ngs.ru>
2016-05-25 22:01:51 +0300

    try runserver

--- init.sh
+++ init.sh
@@ -3,5 +3,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-gunicorn -c /home/box/web/etc/hello.py hello:application --daemon
-gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+#gunicorn -c /home/box/web/etc/hello.py hello:application --daemon
+#gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+
+python /home/box/web/ask/manage.py runserver 0.0.0.0:8000 &
--- vagrant.sh
+++ vagrant.sh
@@ -3,16 +3,18 @@
 # run as sudo
 apt-get update
 apt-get install -y Nginx
-apt-get install -y gunicorn
-apt-get install -y python-pip
-pip install Django==1.6.1
+#apt-get install -y gunicorn
+#apt-get install -y python-pip
+#pip install Django==1.6.1
 apt-get install mc
 apt-get install mysql-server
 
 ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
 
-gunicorn -c /home/box/web/etc/hello.py hello:application --daemon
-gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+#gunicorn -c /home/box/web/etc/hello.py hello:application --daemon
+#gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+
+python /home/box/web/ask/manage.py runserver 0.0.0.0:8000 &
 
 /etc/init.d/mysql start
