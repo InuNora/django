e966d6921955e3695a691227a02a9d8f7bd5327b
Julia Pikareva <sharp@ngs.ru>
2016-05-25 13:01:35 +0300

    Update views.py

--- ask/ask/views.py
+++ ask/ask/views.py
@@ -1,15 +1,7 @@
-from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from django.views.decorators.http import require_GET
+from django.shortcuts import render
+from django.http import HttpResponse
 
-from models import Question
 
-@require_GET
-def question(request, id):
-    print(id)
-    question = get_object_or_404(Question, id=id)
-    print(question.title)
-    return render(request, 'question.html', {
-	'question': question,
-	'answers': question.answer_set.all(),
-	})
+# Create your views here.
+def test(request, *args, **kwargs):
+    return HttpResponse('OK')
