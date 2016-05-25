bfedd9b43aef27e8089b2d0bb043fe004f33b9c6
Julia Pikareva <sharp@ngs.ru>
2016-05-25 14:01:51 +0300

    Update answers.sql

--- scripts/answers.sql
+++ scripts/answers.sql
@@ -1,8 +1,8 @@
-INSERT INTO qa.answers(text, question_id) VALUES ('Upppsss...', 1);
-INSERT INTO qa.answers(text, question_id) VALUES ('Never heard of it', 1);
-INSERT INTO qa.answers(text, question_id) VALUES ('I have no ideas', 2);
-INSERT INTO qa.answers(text, question_id) VALUES ('Becouse', 2);
-INSERT INTO qa.answers(text, question_id) VALUES ('We are alone!!!', 2);
-INSERT INTO qa.answers(text, question_id) VALUES ('Come on and dance with me', 2);
+INSERT INTO ask.answers(text, question_id) VALUES ('Upppsss...', 1);
+INSERT INTO ask.answers(text, question_id) VALUES ('Never heard of it', 1);
+INSERT INTO ask.answers(text, question_id) VALUES ('I have no ideas', 2);
+INSERT INTO ask.answers(text, question_id) VALUES ('Becouse', 2);
+INSERT INTO ask.answers(text, question_id) VALUES ('We are alone!!!', 2);
+INSERT INTO ask.answers(text, question_id) VALUES ('Come on and dance with me', 2);
 
-select * from qa.answers;
+select * from ask.answers;
