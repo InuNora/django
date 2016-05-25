75604c143e1cf734f05b03335b36ba809e08c83c
Julia Pikareva <sharp@ngs.ru>
2016-05-25 14:14:53 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -4,13 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^question/', include('qa.urls'), name='question'),
-    # url(r'^$', 'views.test', name='home'),
-    # url(r'^login/$', 'views.test', name='login'),
-    # url(r'^signup/$', 'views.test', name='signup'),
-    # url(r'^ask/.*$', 'views.test', name='ask'),
-    # url(r'^popular/$', 'views.test', name='popular'),
-    # url(r'^new/$', 'views.test', name='new'),
-    url(r'^admin/', include(admin.site.urls)),
+
     url(r'^', include('qa.urls')),
 )
