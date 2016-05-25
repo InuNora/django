18177402187aded64b84d0c96455ab1d9bed6e5e
Julia Pikareva <sharp@ngs.ru>
2016-05-25 13:33:23 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,19 +13,23 @@ class QuestionManager(models.Manager):
 
 class Question(models.Model):
     objects = QuestionManager()
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=255, db_index=True)
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, related_name="question_author")
-    likes = models.ManyToManyField(User)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(Userб related_name='likes')
     def __unicode__(self):              
         return self.title
+    class Meta:
+        db_table = 'questions'
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now=True)
-    question = models.ForeignKey(Question, null=True)
-    author = models.ForeignKey(User, related_name="answer_author")
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     def __unicode__(self):              # __unicode__ on Python 2
         return self.text
+    class Meta:
+        db_table = 'answers'
